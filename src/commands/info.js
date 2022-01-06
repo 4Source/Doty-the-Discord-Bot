8a3cb0d2e2d860d8035cbadf654d041559543b25
4Source <4Source@users.noreply.github.com>
2022-01-06 16:52:13 +0100

    Info Message only Seen by requester

--- src/commands/info.js
+++ src/commands/info.js
@@ -10,16 +10,16 @@ module.exports = {
 	.addSubcommand(subcommand =>
 		subcommand
 			.setName('user')
-			.setDescription('Info about a user')
+			.setDescription('Info about a User')
 			.addUserOption(option => option.setName('target').setDescription('Select a User')))
 	.addSubcommand(subcommand =>
 		subcommand
 			.setName('server')
-			.setDescription('Info about the server'))
+			.setDescription('Info about the Server'))
     .addSubcommand(subcommand =>
         subcommand
-            .setName('ping')
-            .setDescription('Ping of the Bot')),
+            .setName('bot')
+            .setDescription('Info about the Bot')),
 
     
     async execute(interaction) {
@@ -30,17 +30,17 @@ module.exports = {
                 const user = interaction.options.getUser('target');
                 
                 if(user) {
-                    await interaction.reply(`Username: ${user.tag}\nID: ${interaction.user.id}`);
+                    await interaction.reply({ content: `Username: ${user.tag}\nID: ${interaction.user.id}`, ephemeral: true });
                 } 
                 else {
-                    await interaction.reply(`Your tag: ${interaction.user.tag}\nID: ${interaction.user.id}`);
+                    await interaction.reply({ content: `Your tag: ${interaction.user.tag}\nID: ${interaction.user.id}`, ephemeral: true });
                 }
                 break;
             case 'server':
-                await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+                await interaction.reply({ content: `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`, ephemeral: true });
                 break;
             case 'ping':
-                await interaction.reply(`Ping:  ms.`);
+                await interaction.reply({ content: `Ping: ${interaction.client.ws.ping} ms.`, ephemeral: true });
                 break;
         }
     }
