9dc83a57bbf21f72de122864b2d688d3c0bd68d9
4Source <4Source@users.noreply.github.com>
2022-01-07 00:31:35 +0100

    Added Comments

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,15 +3,21 @@ const short = require('shortid');
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
+        //Create Id for Logging
         const id = short();
+        //Log
 		console.log(`[interactionCreate][#${interaction.channel.name}][${interaction.user.tag}][${id}] Triggerd!`);
         
+        //Check ifnot SlashCommand
         if (!interaction.isCommand()) return;
 
+        //Search in SlashCommands Collection for SlashCommand
         const command = interaction.client.slashCommands.get(interaction.commandName);
 
+        //Check ifnot Valid SlashCommand
         if (!command) return;
 
+        //Execute SlashCommand
         try {
             await command.execute(interaction);
             console.log(`[interactionCreate][#${interaction.channel.name}][${interaction.user.tag}][${id}] Successfully Executed Command!`);
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,8 +4,12 @@ const { prefix } = require("../../data/config.json");
 module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
+		//Ignor Bot Messages
 		if (message.author.bot) return;
+
+        //Create Id for Logging
         const id = short();
+        //Log
 		console.log(`[messageCreate][#${message.channel.name}][${message.author.tag}][${id}] Triggerd with: "${message.content}"`);
 
 		//Check if Command with Prefix
@@ -17,8 +21,10 @@ module.exports = {
 		//Search in Commands Collection for Command
 		const command = message.client.commands.find(cmd => cmd.name == args[0]);
 
+		//Check ifnot Valid Command
 		if(!command) return message.reply({ content: "That is not a valid command!", ephemeral: true });
 
+		//Execute Command
 		try {
             await command.execute(message, args, message.client);
             console.log(`[messageCreate][#${message.channel.name}][${message.author.tag}][${id}] Successfully Executed Command!`);
