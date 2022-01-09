97860e761b7912c07d7924a4507a82cd3c54db76
4Source <4Source@users.noreply.github.com>
2022-01-09 17:11:31 +0100

    Added Database Connection

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require("sequelize");
+
+const db = new Sequelize(
+    process.env.MYSQL_DB_DATABASE, 
+    process.env.MYSQL_DB_USERNAME, 
+    process.env.MYSQL_DB_PASSWORD, {
+    dialect: "mysql",
+    host: process.env.MYSQL_DB_HOST,
+});
+
+module.exports = db;
