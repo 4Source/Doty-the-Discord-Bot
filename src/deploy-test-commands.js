cf5f84a30a51ad250486aa2f6a6272b631258d39
4Source <4Source@users.noreply.github.com>
2022-01-09 13:47:44 +0100

    Added dotenv

--- src/deploy-test-commands.js
+++ src/deploy-test-commands.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('../data/token.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./src/slashCommands').filter(file => file.endsWith('.js'));
@@ -11,14 +10,14 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(process.env.BOT_CLIENT_ID, process.env.BOT_GUILD_ID),
 			{ body: commands },
 		);
 
--- src/index.js
+++ src/index.js
@@ -1,69 +1,74 @@
+require( 'dotenv' ).config();
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require("../data/token.json");
 
 console.log("Starting...");
 //Create Client Instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [
+    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILD_MESSAGES] 
+});
 client.slashCommands = new Collection();
 client.commands = new Collection();
 
 const predir ='./src';
 
-//Load SlashCommands
-scomCount = 0;
-const scomdir = '/slashCommands';
-fs.readdirSync(predir + scomdir)
-    .filter(file => file.endsWith('.js'))
-    .forEach(file => {
-        const command = require(`.${scomdir}/${file}`);
-        if(command.data.name){
-            console.log(`Loaded SlashCommand: '${command.data.name}' from ${predir + scomdir}/${file}`);
-            client.slashCommands.set(command.data.name, command);
-            scomCount++;
-        }
-    });
-fs.readdir(predir + scomdir, (err, files) => {
-    console.log(`SlashCommands Loaded: (${scomCount}/${files.length})`);
-});
-
-//Load Commands
-comCount = 0;
-const comdir = '/commands';
-fs.readdirSync(predir + comdir)
-    .filter(file => file.endsWith('.js'))
-    .forEach(file => {
-        const command = require(`.${comdir}/${file}`);
-        if(command.name) {
-            console.log(`Loaded Command: '${command.name}' from ${predir + comdir}/${file}`);
-            client.commands.set(command.name, command);
-            comCount++;
-        }
-    });
-fs.readdir(predir + comdir, (err, files) => {
-    console.log(`Commands Loaded: (${comCount}/${files.length})`);
-});
-
-//Load Events
-evCount = 0;
-const evdir = '/events';
-fs.readdirSync(predir + evdir)
-    .filter(file => file.endsWith('.js'))
-    .forEach(file => {
-        const event = require(`.${evdir}/${file}`);
-        if(event.name){
-            console.log(`Loaded Event: '${event.name}' from ${predir + evdir}/${file}`);
-            if (event.once) {
-                client.once(event.name, (...args) => event.execute(...args));
-            } else {
-                client.on(event.name, (...args) => event.execute(...args));
+(async () => {
+    //Load SlashCommands
+    scomCount = 0;
+    const scomdir = '/slashCommands';
+    await fs.readdirSync(predir + scomdir)
+        .filter(file => file.endsWith('.js'))
+        .forEach(file => {
+            const command = require(`.${scomdir}/${file}`);
+            if(command.data.name){
+                console.log(`Loaded SlashCommand: '${command.data.name}' from ${predir + scomdir}/${file}`);
+                client.slashCommands.set(command.data.name, command);
+                scomCount++;
             }
-            evCount++;
-        }
+        });
+    await fs.readdir(predir + scomdir, (err, files) => {
+        console.log(`SlashCommands Loaded: (${scomCount}/${files.length})`);
     });
-fs.readdir(predir + evdir, (err, files) => {
-    console.log(`Events Loaded: (${evCount}/${files.length})`);
-});
 
-//Login with Discord Token
-client.login(token);
+    //Load Commands
+    comCount = 0;
+    const comdir = '/commands';
+    await fs.readdirSync(predir + comdir)
+        .filter(file => file.endsWith('.js'))
+        .forEach(file => {
+            const command = require(`.${comdir}/${file}`);
+            if(command.name) {
+                console.log(`Loaded Command: '${command.name}' from ${predir + comdir}/${file}`);
+                client.commands.set(command.name, command);
+                comCount++;
+            }
+        });
+    await fs.readdir(predir + comdir, (err, files) => {
+        console.log(`Commands Loaded: (${comCount}/${files.length})`);
+    });
+
+    //Load Events
+    evCount = 0;
+    const evdir = '/events';
+    await fs.readdirSync(predir + evdir)
+        .filter(file => file.endsWith('.js'))
+        .forEach(file => {
+            const event = require(`.${evdir}/${file}`);
+            if(event.name){
+                console.log(`Loaded Event: '${event.name}' from ${predir + evdir}/${file}`);
+                if (event.once) {
+                    client.once(event.name, (...args) => event.execute(...args));
+                } else {
+                    client.on(event.name, (...args) => event.execute(...args));
+                }
+                evCount++;
+            }
+        });
+    await fs.readdir(predir + evdir, (err, files) => {
+        console.log(`Events Loaded: (${evCount}/${files.length})`);
+    });
+
+    //Login with Discord Token
+    client.login(process.env.BOT_TOKEN);
+})();
