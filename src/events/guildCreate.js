a636c7a6bac03eb4de147ea885f307867963165a
4Source <4Source@users.noreply.github.com>
2022-01-13 18:52:07 +0100

    Change GuildCreate set in mem

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -8,7 +8,7 @@ module.exports = {
 				guild_id: guild.id
 			}
 		});
-		//guild.client.guildConfigs.set(config.guild_id, config.dataValues);
+		guild.client.guildConfigs.set(config.guild_id, config.dataValues);
 		console.log(created ? `Joined new Guild ${guild.name} with Guild ID: ${config.guild_id}` : `Rejoind to Guild ${guild.name} with Guild ID: ${config.guild_id}`);
 	},
 };
